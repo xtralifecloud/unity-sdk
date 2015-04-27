7ae76257e0874a4825523b6b9b9af064b727aa0a
Florian <florian@clanofthecloud.com>
2015-04-27 12:34:08 +0900

    CloudBuilder Unity first version (uses asynchronous HTTP, in another thread -- might lock the main thread)

---
+++ CloudBuilderUnity/Assets/Scripts/CloudBuilder.cs
@@ -0,0 +1,59 @@
+﻿using UnityEngine;
+using System.Collections;
+using System;
+
+namespace CloudBuilderLibrary {
+	public class CloudBuilder {
+		private static CClan clanInstance;
+		private static IHttpClient httpClientInstance;
+		private static ILogger loggerInstance;
+		private static ISystemFunctions systemFunctionsInstance;
+		// TODO
+		internal const string Version = "1";
+
+		public static CClan Clan {
+			get { return clanInstance; }
+		}
+
+		// You need to call this from the update of your current scene!
+/*		public static void Update() {
+			// TODO gérer le timeout de http
+		}*/
+
+		public static void Log(string text) {
+			loggerInstance.Log(LogLevel.Verbose, text);
+		}
+		public static void Log(LogLevel level, string text) {
+			loggerInstance.Log(level, text);
+		}
+		public static void StartLogTime(string description = null) {
+			initialTicks = DateTime.UtcNow.Ticks;
+			LogTime(description);
+		}
+		public static void LogTime(string description = null) {
+			TimeSpan span = new TimeSpan(DateTime.UtcNow.Ticks - initialTicks);
+			loggerInstance.Log(LogLevel.Verbose, "[" + span.TotalMilliseconds  + "] " + description);
+		}
+
+		#region Internal stuff
+		static CloudBuilder() {
+			clanInstance = new CClan();
+			httpClientInstance = new UnityHttpClient();
+			loggerInstance = UnityLogger.Instance;
+			systemFunctionsInstance = new UnitySystemFunctions();
+        }
+
+		internal static IHttpClient HttpClient {
+			get { return httpClientInstance; }
+		}
+
+		internal static ISystemFunctions SystemFunctions {
+			get { return systemFunctionsInstance; }
+		}
+		#endregion
+
+		#region Private
+		private static long initialTicks;
+		#endregion
+	}
+}
---
+++ CloudBuilderUnity/Assets/TestNewCloudBuilder.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+using CloudBuilderLibrary;
+using LitJson;
+
+public class TestNewCloudBuilder : MonoBehaviour {
+
+	// Use this for initialization
+	void Start() {
+		JsonData config = new JsonData();
+		config["key"] = "cloudbuilder-key";
+		config["secret"] = "azerty";
+		config["env"] = "http://195.154.227.44:8000";
+		config["httpVerbose"] = true;
+		CloudBuilder.Clan.Setup(config);
+	}
+	
+	// Update is called once per frame
+	void Update() {
+	
+	}
+
+	public void DoLogin() {
+		CloudBuilder.Clan.LoginAnonymous();
+    }
+}
