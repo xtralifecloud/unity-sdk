c9cf51d8f7823d589c8236cce070128f8bc8fb7c
Florian B <f_bron@hotmail.com>
2015-05-08 09:41:52 +0900

    Facebook integration now saves data among instances

---
+++ CloudBuilderUnity/Assets/CloudBuilder/Editor/Modules/Facebook/CloudBuilderFacebookIntegrationEditor.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+using CloudBuilderLibrary;
+using System.Collections.Generic;
+using System;
+using UnityEditor;
+
+namespace CloudBuilderLibrary
+{
+	[CustomEditor(typeof(CloudBuilderFacebookIntegration))]
+	public class CloudBuilderFacebookIntegrationEditor : Editor {
+
+		public override void OnInspectorGUI() {
+			CloudBuilderFacebookIntegration inst = target as CloudBuilderFacebookIntegration;
+			EditorGUILayout.BeginVertical();
+			inst.AppId = EditorGUILayout.TextField("Facebook App ID", inst.AppId);
+			EditorGUILayout.EndVertical();
+		}
+
+	}
+}
