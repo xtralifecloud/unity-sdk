cce8498ce2818dfb6963b0e1f974272695711242
Florian B <f_bron@hotmail.com>
2015-05-11 19:54:30 +0900

    Forgot new files

---
+++ CloudBuilderUnity/Assets/Scripts/CLI/Commands.Basic.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using CloudBuilderLibrary;
+
+namespace CLI
+{
+    public partial class Commands {
+
+		[CommandInfo("Logs in anonymously and starts the event loop.")]
+		void loginanonymous(Arguments args) {
+			Clan.LoginAnonymously(SuccessHandler<Gamer>(DidLogin));
+		}
+
+		[CommandInfo("Resumes an existing session by gamer ID/secret.", "gamer_id, gamer_secret")]
+		void resumesession(Arguments args) {
+			args.Expecting(2, ArgumentType.String, ArgumentType.String);
+			Clan.ResumeSession(
+				done: SuccessHandler<Gamer>(DidLogin),
+				gamerId: args.StringArg(0),
+				gamerSecret: args.StringArg(1));
+		}
+
+		/**
+		 * This handler handles any login done message.
+		 */
+		private void DidLogin(Result<Gamer> result) {
+			Gamer = result.Value;
+			// Only one loop at a time
+			if (PrivateEventLoop != null) PrivateEventLoop.Stop();
+			PrivateEventLoop = new DomainEventLoop(Gamer);
+		}
+    }
+}
---
+++ CloudBuilderUnity/Assets/Scripts/CLI/Commands.Match.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using CloudBuilderLibrary;
+
+namespace CLI
+{
+    public partial class Commands {
+
+		[CommandInfo("Does nothing good yet.", "name[, num_players]")]
+		void match_create(Arguments args) {
+			args.Expecting(1, ArgumentType.String, ArgumentType.Double);
+			Log("Here: " + args.StringArg(0) + ", " + args.DoubleArg(1));
+		}
+
+	}
+}
