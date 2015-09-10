f6e14f61c1ad563535b345f3ed784eb600d9e08c
Florian B <f_bron@hotmail.com>
2015-09-10 09:38:20 +0200

    Regenerated documentation

---
+++ Docs/DoxygenGenerated/html/search/groups_2.js
@@ -0,0 +1,5 @@
+var searchData=
+[
+  ['match_20functionality',['Match functionality',['../group__match__classes.html',1,'']]],
+  ['model_20classes_20_28server_2fclient_20communication_29',['Model classes (server/client communication)',['../group__model__classes.html',1,'']]]
+];
---
+++ Docs/DoxygenGenerated/html/search/groups_4.js
@@ -0,0 +1,4 @@
+var searchData=
+[
+  ['various_20classes_20for_20sdk_20usage',['Various classes for SDK usage',['../group__data__classes.html',1,'']]]
+];
