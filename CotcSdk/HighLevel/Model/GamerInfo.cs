be0df0b9d9695ab01bea3f1c62a61812139fc4a5
Florian <florian@clanofthecloud.com>
2016-01-18 15:00:44 +0100

    Fixed typo in doc

--- CotcSdk/HighLevel/Model/GamerInfo.cs
+++ CotcSdk/HighLevel/Model/GamerInfo.cs
@@ -6,7 +6,7 @@ namespace CotcSdk {
 	/// Info about a player.
 	/// Can be enriched with information, accessible using the index operator [].
 	/// Typically contains a profile field, with displayname, email and lang. You can fetch this by doing
-	/// `string name = GamerInfo["profile"]["displayname"];`
+	/// `string name = GamerInfo["profile"]["displayName"];`
 	/// </summary>
 	public class GamerInfo: PropertiesObject {
 		/// <summary>Id of the gamer.</summary>
