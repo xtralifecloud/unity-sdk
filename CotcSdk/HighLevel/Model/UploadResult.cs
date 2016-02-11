44fa1acaaf4e63dd901c7f1b3ee57fb954d14389
Florian B <f_bron@hotmail.com>
2016-02-11 10:39:56 +0100

    Added UploadResult for GamerVfs.SetKeyBinary

---
+++ CotcSdk/HighLevel/Model/UploadResult.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace CotcSdk {
+
+	/// <summary>
+	/// If an object of this type is returned, then it means that the thing was uploaded successfully.
+	/// You can get the URL using the Url member.
+	/// </summary>
+	public class UploadResult: PropertiesObject {
+		/// <summary>The URL to which the content was uploaded.</summary>
+		public string Url { get; private set; }
+
+		public UploadResult(Bundle serverData, string url) : base(serverData) {
+			Url = url;
+		}
+	}
+}
