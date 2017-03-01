feba3f1640c4dc812d145eb94a157e198ff7840b
Sebastien Didierdefresse <sebastien@xtralife.cloud>
2017-03-01 15:39:21 +0100

    Add PushNotification constructor overloads

--- CotcSdk/HighLevel/Model/PushNotification.cs
+++ CotcSdk/HighLevel/Model/PushNotification.cs
@@ -1,4 +1,5 @@
-﻿
+﻿using System.Collections.Generic;
+
 namespace CotcSdk {
 
 	/// @ingroup data_classes
@@ -9,9 +10,43 @@ namespace CotcSdk {
 	/// </summary>
 	public class PushNotification {
 
-		/// <summary>Default constructor.</summary>
+		/// <summary>Creates a PushNotification object.</summary>
+		/// <returns>A new PushNotification object.</returns>
 		public PushNotification() {
 			Data = Bundle.CreateObject();
+		}
+
+		/// <summary>Creates a PushNotification object with one language/text pair which will be put in the object initially.</summary>
+		/// <returns>A new bundle filled with one language/text pair.</returns>
+		public PushNotification(string language, string text) {
+			Data = Bundle.CreateObject();
+			Data[language] = text;
+		}
+
+		/// <summary>Creates a PushNotification object with two language/text pairs which will be put in the object initially.</summary>
+		/// <returns>A new bundle filled with two language/text pairs.</returns>
+		public PushNotification(string language1, string text1, string language2, string text2) {
+			Data = Bundle.CreateObject();
+			Data[language1] = text1;
+			Data[language2] = text2;
+		}
+
+		/// <summary>Creates a PushNotification object with three language/text pairs which will be put in the object initially.</summary>
+		/// <returns>A new bundle filled with three language/text pairs.</returns>
+		public PushNotification(string language1, string text1, string language2, string text2, string language3, string text3) {
+			Data = Bundle.CreateObject();
+			Data[language1] = text1;
+			Data[language2] = text2;
+			Data[language3] = text3;
+		}
+
+		/// <summary>Creates a PushNotification object with many language/text pairs which will be put in the object initially.</summary>
+		/// <returns>A new bundle filled with many language/text pairs.</returns>
+		public PushNotification(params KeyValuePair<string, string>[] languageTextPairs) {
+			Data = Bundle.CreateObject();
+			foreach (KeyValuePair<string, string> languageTextPair in languageTextPairs) {
+				Data[languageTextPair.Key] = languageTextPair.Value;
+			}
 		}
 
 		/// <summary>Adds or replaces a string for a given language.</summary>
