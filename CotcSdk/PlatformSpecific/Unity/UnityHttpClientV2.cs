11e743918d9ebd9cd54b9b3a17bbb59cceed5a8d
Michael El Baki <michael@xtralife.cloud>
2017-05-03 11:40:20 +0200

    UnityWebRequest on iOS behaves differently from other platforms in case of network error.

--- CotcSdk/PlatformSpecific/Unity/UnityHttpClientV2.cs
+++ CotcSdk/PlatformSpecific/Unity/UnityHttpClientV2.cs
@@ -92,39 +92,40 @@ namespace CotcSdk {
             private IEnumerator ProcessRequest(UnityWebRequest req) {
                 yield return req.Send();
 
-                if (req.isError) {
+                if (req.isError)
+                {
                     Debug.Log("Request error: " + req.error);
-                    if (!WasAborted) {
+                    if (!WasAborted)
+                    {
                         string errorMessage = "Failed web request: " + req.error;
                         Common.Log(errorMessage);
                         self.FinishWithRequest(this, new HttpResponse(new Exception(errorMessage)));
                     }
                 }
-                else {
+                else
+                {
                     // Extracts asset bundle
                     HttpResponse response = new HttpResponse();
                     response.Body = Request.downloadHandler.data;
                     response.StatusCode = (int)Request.responseCode;
                     Debug.Log("Status code: " + response.StatusCode);
                     Dictionary<string, string> dict = Request.GetResponseHeaders();
-#if UNITY_IOS
-                    // Nice bug in the iOS UnityWebRequest implementation. When you abort a request, in fact it doesn't
-                    // abort the request. Instead of receiving an error (req.isError and req.error can check that), you
-                    // just receive a funny responseCode (like 0 or -1001) and an empty dictionary of response headers.
-                    if (response.StatusCode <= 0 && dict == null)
+                    if (Application.platform == RuntimePlatform.IPhonePlayer && response.StatusCode <= 0 /*&& dict == null*/)
+                    {
+                        // Nice bug in the iOS UnityWebRequest implementation. When you abort a request, in fact it doesn't
+                        // abort the request. Instead of receiving an error (req.isError and req.error can check that), you
+                        // just receive a funny responseCode (like 0 or -1001) and an empty dictionary of response headers.
                         Debug.Log("Unity iOS bug for UnityWebRequest...");
+                    }
                     else
                     {
-#endif
                         foreach (var pair in dict)
                         {
                             response.Headers[pair.Key] = pair.Value;
                         }
                         LogResponse(response);
                         self.FinishWithRequest(this, response);
-#if UNITY_IOS
                     }
-#endif
                 }
             }
 
