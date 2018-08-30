6eb93e8f73ce910e421c9e5f89e9dcd124ea1ef3
Sebastien Didierdefresse <sebastien@xtralife.cloud>
2018-08-30 13:00:59 +0200

    Remove unused Monodevelop's after-build CustomCommands scripts as it uses projects' PostBuildEvents to do the same work

--- CotcSdk/After-build-release.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/sh
-mkdir -p ../UnityProject/Assets/Plugins
-cp bin/Release/CotcSdk.dll ../UnityProject/Assets/Plugins/CotcSdk.dll
-cp bin/Release/CotcSdk.xml ../UnityProject/Assets/Plugins/CotcSdk.xml
--- CotcSdk/After-build.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/sh
-mkdir -p ../UnityProject/Assets/Plugins
-cp bin/Debug/CotcSdk.dll ../UnityProject/Assets/Plugins/CotcSdk-Debug.dll
-cp bin/Debug/CotcSdk.xml ../UnityProject/Assets/Plugins/CotcSdk-Debug.xml
